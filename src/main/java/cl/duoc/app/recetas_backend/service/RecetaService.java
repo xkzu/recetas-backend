0e35116f04f78dc3407d2f9e204fde5afbe73a71
Kzu <alvaro.vega.pavez@gmail.com>
2024-11-03 06:40:05 -0300

    service receta

---
+++ src/main/java/cl/duoc/app/recetas_backend/service/RecetaService.java
@@ -0,0 +1,19 @@
+package cl.duoc.app.recetas_backend.service;
+
+import cl.duoc.app.recetas_backend.model.Receta;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface RecetaService {
+
+    List<Receta> findAll();
+
+    Optional<Receta> findById(Long id);
+
+    Optional<Receta> findByName(String name);
+
+    List<Receta> findByCountry(String country);
+
+    List<Receta> obtenerRecetasMasRecientes(int limite);
+}
