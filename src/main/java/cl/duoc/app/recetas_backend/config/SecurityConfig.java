55beabb7e99ccd5fb29623e092cb7d10deb4f9aa
Kzu <alvaro.vega.pavez@gmail.com>
2024-11-03 06:39:20 -0300

    config de los path permitidos

---
+++ src/main/java/cl/duoc/app/recetas_backend/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package cl.duoc.app.recetas_backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorizeRequests ->
+                        authorizeRequests
+                                .requestMatchers("/recetas/recientes/**").permitAll()
+                                .requestMatchers("/recetas/all").permitAll()
+                                .requestMatchers("/h2-console/**").permitAll()
+                                .anyRequest().authenticated()
+                )
+                .csrf(csrf -> csrf.disable())
+                .headers(headers -> headers.frameOptions(config -> config.sameOrigin()));
+
+        return http.build();
+    }
+}
