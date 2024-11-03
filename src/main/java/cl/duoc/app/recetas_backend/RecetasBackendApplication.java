e84f25399f61fd3fc2ad04529b344166e090c4c6
Kzu <alvaro.vega.pavez@gmail.com>
2024-11-03 01:21:58 -0300

    estructura básica

---
+++ src/main/java/cl/duoc/app/recetas_backend/RecetasBackendApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.app.recetas_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecetasBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecetasBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/duoc/app/recetas_backend/RecetasBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.app.recetas_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecetasBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
