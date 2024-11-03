2f464d1a3fac49c812e94d62f7f5046fee535595
Kzu <alvaro.vega.pavez@gmail.com>
2024-11-03 06:40:35 -0300

    controller receta

---
+++ src/main/java/cl/duoc/app/recetas_backend/controller/RecetaController.java
@@ -0,0 +1,39 @@
+package cl.duoc.app.recetas_backend.controller;
+
+import cl.duoc.app.recetas_backend.model.Receta;
+import cl.duoc.app.recetas_backend.service.RecetaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/recetas")
+public class RecetaController {
+
+    private final RecetaService recetaService;
+
+    @Autowired
+    public RecetaController(RecetaService recetaService) {
+        this.recetaService = recetaService;
+    }
+
+    @GetMapping("/recientes/{limite}")
+    public ResponseEntity<List<Receta>> recientes(@PathVariable int limite) {
+        try {
+            return ResponseEntity.ok(recetaService.obtenerRecetasMasRecientes(limite));
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().build();
+        }
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<Receta>> getAllRecetas() {
+        return ResponseEntity.ok(recetaService.findAll());
+    }
+
+}
