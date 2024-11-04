da39a1dc903ed1885f0aaf289654cc7e6ee97075
Kzu <alvaro.vega.pavez@gmail.com>
2024-11-04 05:51:41 -0300

    feat: update tablas en schema

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -12,3 +12,20 @@ CREATE TABLE IF NOT EXISTS receta (
     fotografia_url VARCHAR(255),
     fecha_creacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     );
+
+CREATE TABLE IF NOT EXISTS usuario (
+                                       id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                                       nombre_usuario VARCHAR(255) UNIQUE NOT NULL,
+    contrasena VARCHAR(255) NOT NULL,
+    rol BOOLEAN
+    );
+
+CREATE TABLE IF NOT EXISTS banner (
+                                      id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                                      empresa_nombre VARCHAR(255) NOT NULL,
+    mensaje VARCHAR(1000) NOT NULL,
+    url_imagen VARCHAR(255),
+    link_website VARCHAR(255),
+    fecha_creacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );
+
