b4975b64c30f7d0d1788f525b22b85a7c4a4e22b
Kzu <alvaro.vega.pavez@gmail.com>
2024-11-04 05:56:04 -0300

    feat: update se agrega metodo buscarRecetas

--- src/main/java/cl/duoc/app/recetas_backend/repository/RecetaRepository.java
+++ src/main/java/cl/duoc/app/recetas_backend/repository/RecetaRepository.java
@@ -4,6 +4,8 @@ import cl.duoc.app.recetas_backend.model.Receta;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -23,4 +25,16 @@ public interface RecetaRepository extends JpaRepository<Receta, Long> {
     }
 
     List<Receta> findAllByOrderByPopularidadDesc(Pageable pageable);
+
+    @Query("SELECT r FROM Receta r " +
+            "WHERE (:nombre IS NULL OR LOWER(r.nombre) LIKE LOWER(CONCAT('%', :nombre, '%'))) " +
+            "AND (:tipoCocina IS NULL OR LOWER(r.tipoCocina) LIKE LOWER(CONCAT('%', :tipoCocina, '%'))) " +
+            "AND (:ingredientes IS NULL OR LOWER(r.ingredientes) LIKE LOWER(CONCAT('%', :ingredientes, '%'))) " +
+            "AND (:paisOrigen IS NULL OR LOWER(r.paisOrigen) LIKE LOWER(CONCAT('%', :paisOrigen, '%'))) " +
+            "AND (:dificultad IS NULL OR LOWER(r.dificultad) LIKE LOWER(CONCAT('%', :dificultad, '%')))")
+    List<Receta> buscarRecetas(@Param("nombre") String nombre,
+                               @Param("tipoCocina") String tipoCocina,
+                               @Param("ingredientes") String ingredientes,
+                               @Param("paisOrigen") String paisOrigen,
+                               @Param("dificultad") String dificultad);
 }
