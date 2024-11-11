50bad0f98f654a932594629409216856268a972a
Kzu <alvaro.vega.pavez@gmail.com>
2024-11-11 06:37:00 -0300

    feat: update UsuarioController, se crea metodo register y se actualiza metodo login, comparando la contraseña con una encriptada

--- src/main/java/cl/duoc/app/recetas_backend/controller/UsuarioController.java
+++ src/main/java/cl/duoc/app/recetas_backend/controller/UsuarioController.java
@@ -5,9 +5,11 @@ import cl.duoc.app.recetas_backend.model.Usuario;
 import cl.duoc.app.recetas_backend.response.LoginResponse;
 import cl.duoc.app.recetas_backend.service.UsuarioService;
 import cl.duoc.app.recetas_backend.util.JwtUtil;
+import cl.duoc.app.recetas_backend.util.Util;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,21 +23,45 @@ public class UsuarioController {
 
     private final UsuarioService usuarioService;
     private final JwtUtil jwtUtil;
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public UsuarioController(UsuarioService usuarioService, JwtUtil jwtUtil) {
+    public UsuarioController(UsuarioService usuarioService, JwtUtil jwtUtil, PasswordEncoder passwordEncoder) {
         this.usuarioService = usuarioService;
         this.jwtUtil = jwtUtil;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @PostMapping("/login")
-    public ResponseEntity<?> login(@RequestBody UsuarioReq user) {
-        Optional<Usuario> usuario = usuarioService.login(user.getNombre(), user.getPassword());
+    public ResponseEntity<LoginResponse> login(@RequestBody UsuarioReq user) {
+        Optional<Usuario> usuario = usuarioService.findByNombreUsuario(user.getNombre());
+
         if (usuario.isPresent()) {
-            String token = jwtUtil.generateToken(user.getNombre());
-            return ResponseEntity.ok(new LoginResponse(usuario, token));
+            if (passwordEncoder.matches(user.getPassword(), usuario.get().getContrasena())) {
+                String token = jwtUtil.generateToken(user.getNombre());
+                return ResponseEntity.ok(new LoginResponse(usuario, token));
+            } else {
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+            }
         } else {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Credenciales incorrectas");
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<Usuario> register(@RequestBody Usuario user) {
+        try {
+            if (Util.isEmptyOrNull(user.getNombreUsuario())
+                    || Util.isEmptyOrNull(user.getNombreCompleto())
+                    || Util.isEmptyOrNull(user.getEmail())
+                    || Util.isEmptyOrNull(user.getContrasena())) {
+                return ResponseEntity.badRequest().build();
+            }
+            String encryptedPassword = passwordEncoder.encode(user.getContrasena());
+            user.setContrasena(encryptedPassword);
+            return ResponseEntity.ok(usuarioService.register(user));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
     }
 
