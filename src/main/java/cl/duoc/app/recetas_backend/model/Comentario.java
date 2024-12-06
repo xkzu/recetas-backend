88eea33d53740f8baccf6da7c301310cc02cf6d6
Kzu <alvaro.vega.pavez@gmail.com>
2024-12-06 06:29:08 -0300

    update: constructores manejados por lombok

--- src/main/java/cl/duoc/app/recetas_backend/model/Comentario.java
+++ src/main/java/cl/duoc/app/recetas_backend/model/Comentario.java
@@ -6,12 +6,16 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Table(name = "comentarios")
 public class Comentario {
 
